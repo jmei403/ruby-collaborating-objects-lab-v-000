7936e5041b57a375bd276c8e0a869e605d66e090
Jason Mei <jason.mei403@gmail.com>
2018-06-25 19:59:19 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,11 +6,11 @@ class MP3Importer
   end
 
   def files
-    Dir.entries(path).select { |file| file.include?(".mp3") }
+    @files ||= Dir.entries(path).select { |file| file.include?(".mp3") }
   end
 
   def import
-    self.files.each { |file| Song.new_by_filename(file) }
+    files.each { |file| Song.new_by_filename(file) }
   end
 
 end
